75e51b1a44de0febcc6b9f4054965624622fdb6c
Valentin G <deathman777@gmail.com>
2024-11-24 17:55:47 +0500

    fixes with agent test and action.yml

--- src/main/java/ru/gordeev/teamcity/api/requests/non_crud/checked/CheckedAgentAuthorizeRequest.java
+++ src/main/java/ru/gordeev/teamcity/api/requests/non_crud/checked/CheckedAgentAuthorizeRequest.java
@@ -2,23 +2,24 @@ package ru.gordeev.teamcity.api.requests.non_crud.checked;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import ru.gordeev.teamcity.api.enums.Endpoint;
 import ru.gordeev.teamcity.api.requests.EndpointActions;
 import ru.gordeev.teamcity.api.requests.Request;
 import ru.gordeev.teamcity.api.requests.non_crud.AgentAuthorizeInterface;
 
-public class CheckedAgentAuthorizeRequest extends Request implements AgentAuthorizeInterface<Response>, EndpointActions {
+public class CheckedAgentAuthorizeRequest extends Request implements AgentAuthorizeInterface<ValidatableResponse>, EndpointActions {
 
     public CheckedAgentAuthorizeRequest(RequestSpecification spec, Endpoint endpoint) {
         super(spec, endpoint);
     }
 
     @Override
-    public Response authorizeAgent(String name, boolean isAuthorize) {
-        return (Response) RestAssured
+    public ValidatableResponse authorizeAgent(String name, boolean isAuthorize) {
+        return RestAssured
                 .given()
                 .spec(spec)
                 .accept(ContentType.ANY)
@@ -27,6 +28,8 @@ public class CheckedAgentAuthorizeRequest extends Request implements AgentAuthor
                 .body(isAuthorize)
                 .put(endpoint.getUrl())
                 .then()
-                .assertThat().statusCode(HttpStatus.SC_OK);
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body(Matchers.containsString("true"));
     }
 }
